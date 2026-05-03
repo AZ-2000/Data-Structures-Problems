fae92c932ee614882d3b61cb4373abe20660c5d9
Ammar <134650005+AZ-2000@users.noreply.github.com>
2026-05-03 14:12:30 +1000

    Add: reverse-nodes-in-k-group - submission-2

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.py
@@ -0,0 +1,67 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def print_list(self, node):
+        curr = node
+        while curr:
+            print(curr.val, end= ' ')
+            curr = curr.next
+        print('\n')
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        curr = head
+        count = 0
+        dummy_tail = ListNode(0)
+        dummy_tail.next = head
+        old_tail = None
+
+        while curr:
+            count += 1
+            curr = curr.next
+        
+        if count < k:
+            return head
+        else:
+            curr = head
+            groups = count // k
+            flag = True
+
+            if count % groups != 0:
+                flag = False
+            
+            print(groups)
+
+            for grp in range(groups):
+                j = 0
+                if not flag and grp == groups:
+                    print(grp)
+                    break
+
+                else:
+                    prev = None
+                    
+                    while curr and j < k:
+                        if j == 0:
+                            tail = curr
+                        tmp = curr.next
+                        curr.next = prev
+                        prev = curr
+                        curr = tmp
+                        j += 1 
+                    if old_tail:
+                        old_tail.next = prev
+                    old_tail = tail
+                    if grp == 0:
+                        new_head = prev
+
+                    prev = tail
+                    prev.next = tmp
+                    self.print_list(new_head)
+
+            return new_head
+
+
