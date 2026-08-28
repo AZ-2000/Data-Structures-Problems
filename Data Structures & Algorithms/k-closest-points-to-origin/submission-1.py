0d4c86baaf18f241baed7680674ba9ae11352380
Ammar <134650005+AZ-2000@users.noreply.github.com>
2026-08-28 12:52:22 +1000

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,35 @@
+import heapq
+
+def euclidean_distance(arr1, arr2):
+    x = (arr1[0]-arr2[0])**2
+    print(x, arr1[0], arr2[0])
+    y = (arr1[1] -arr2[1]) **2
+    print(y,arr1[1],arr2[1])
+    distance = (x+y) ** 0.5
+    return distance
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = []
+        res = []
+        for i in range(len(points)):
+            distance = euclidean_distance(points[i], [0,0])
+            print(distance)
+            distances.append((distance, points[i]))
+        
+        heapq.heapify_max(distances)
+        print(distances)
+        while len(distances) > k:
+            heapq.heappop_max(distances)
+
+        for i in range(k):
+            res.append(distances[i][1])
+        
+        return res
+
+
+        
+
+
+
+        
