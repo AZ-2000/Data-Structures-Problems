999eb7493b7623c7a1c7f15a885b2d0aaff7a6f1
Ammar <134650005+AZ-2000@users.noreply.github.com>
2026-09-04 20:06:25 +1000

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.py
@@ -0,0 +1,16 @@
+def dfs(adj, source, res):
+    while adj[source]:
+        neighbour = adj[source].pop()
+        dfs(adj,neighbour,res)
+    res.append(source)
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        tickets.sort(reverse=True)
+        for src, dest in tickets:
+            adj[src].append(dest)
+        res = []
+        dfs(adj,"JFK",res)
+        return res[::-1]
+        
+
